15f641a84e70eef2fbcd3aa582af7915ba02e6ab
nobodyinvited <yatsunenko.serhii@hneu.net>
2026-05-01 19:16:53 +0200

    fix: replace the object elements in the correct order

--- src/components/Header/Links.jsx
+++ src/components/Header/Links.jsx
@@ -3,9 +3,9 @@ import { profile } from "../../data/profile";
 export default function Link() {
   return profile.links.map((item) => (
       <a
-        key={item.link}
-        className="hover:text-blue-600 transition-all"
-        href={item.link}
+        key={item.type}
+        className="hover:text-blue-600 transition-all cursor-pointer"
+        href={item.url}
         rel="noopener noreferrer"
         target="_blank"
       >
