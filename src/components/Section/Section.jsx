7eb4730b2f8a9ec2a5785e4d495ed0b6231b0020
nobodyinvited <yatsunenko.serhii@hneu.net>
2026-05-03 14:25:11 +0200

    feat: implement Section template

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,8 +1,10 @@
-export default function Section({ id, title, subtitle, children}) {
+export default function Section({ id, title, subtitle, children }) {
   return (
     <section className="p-8 flex flex-col gap-2" id={id}>
-      <h2 className="text-black font-bold text-center text-5xl">{title}</h2>
-      <p className="text-center text-gray-400 text-xl">{subtitle}</p>
+      <div className="p-16">
+        <h2 className="text-black font-bold text-center text-5xl">{title}</h2>
+        <p className="text-center text-gray-400 text-xl">{subtitle}</p>
+      </div>
       {children}
     </section>
   );
---
+++ src/components/Section/components/Container.jsx
@@ -0,0 +1,19 @@
+export default function Container({
+  title = "",
+  icon = "",
+  description = "",
+  children,
+}) {
+  return (
+    <div className="w-96 p-8 bg-white shadow-md rounded-2xl border border-gray-200 justify-between hover:scale-105 transition-all">
+      {/* Header for each container with icon if set */}
+      <div className="flex flex-row items-center gap-2 py-2 text-blue-600">
+        <h2 className="font-semibold text-3xl">{title}</h2>
+        <span>{icon}</span>
+      </div>
+      <p className="text-gray-400 py-2">{description}</p>
+      {/* Customized jsx code for unique containers */}
+      {children}
+    </div>
+  );
+}
