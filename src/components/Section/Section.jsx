c39c4b0286a44bdb3b1723eda65a422d721302c5
nobodyinvited <yatsunenko.serhii@hneu.net>
2026-05-06 12:22:32 +0200

    feat: add padding-y for subtitle

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,7 +3,7 @@ export default function Section({ id, title, subtitle, children }) {
     <section className="p-8 flex flex-col gap-2" id={id}>
       <div className="p-16">
         <h2 className="text-black font-bold text-center text-5xl">{title}</h2>
-        <p className="text-center text-gray-400 text-xl">{subtitle}</p>
+        <p className="text-center py-2 text-gray-400 text-xl">{subtitle}</p>
       </div>
       {children}
     </section>
